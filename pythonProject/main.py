d688f6d1085ca87f11ac9b3ddbd9233d881a6089
jake <jake.c.abbey@gmail.com>
2022-07-29 15:08:24 -0700

    New Beginning - w/ Selenium

---
+++ pythonProject/main.py
@@ -0,0 +1,8 @@
+import requests
+
+downloadUrl = 'https://binaries.templates.cdn.office.net/support/templates/en-us/tf16402488_win32.dotx'
+
+req = requests.get(downloadUrl)
+
+
+
---
+++ pythonProject1/main.py
@@ -0,0 +1,10 @@
+num = [1, 2, 3, 1]
+print(len(num))
+i = 0
+print(i)
+while i < len(num):
+    p = num[i]
+    for x in num:
+        if map
+
+    i = i + 1
