509063d19a79ef663011e101fdda0318c13c14f8
jake <jake.c.abbey@gmail.com>
2022-07-29 13:16:15 -0700

    Just starting

--- UGDownloader/main.py
+++ UGDownloader/main.py
@@ -1,15 +1,13 @@
-import requests
-from tkinter import *
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-window = Tk()
-textVar = StringVar()
-label = Label(window, textvariable=textVar)
-textVar.set("Enter artist name:")
-
-textBox = Entry(window)
-button = Button(window, text ='Download')
-label.pack()
-textBox.pack()
-button.pack()
-window.mainloop()
-
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
