612ac933b5d11ef5f553835c1a8345341ad13bfd
jake <jake.c.abbey@gmail.com>
2022-08-04 18:52:45 -0700

    GUI construction

--- UGDownloader/GUI.py
+++ UGDownloader/GUI.py
@@ -1,25 +1,54 @@
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+import PySimpleGUI as sg
+
+# sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+
+# layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+#
+# # Create the window
+# window = sg.Window("Demo", layout, margins=(300, 300))
+#
+# # Create an event loop
+# while True:
+#     event, values = window.read()
+#     # End program if user closes window or
+#     # presses the OK button
+#     if event == "OK" or event == sg.WIN_CLOSED:
+#         break
+#
+# window.close()
 
 
-class GUI:
+# First the window layout in 2 columns
 
-    def __init__(self, driver):
-        self.driver = driver
+left_column = [
+    [sg.In(size=(25, 1), enable_events=True, key="-ARTIST-")],
+    [sg.In(size=(25, 1), enable_events=True, key="-USERNAME-")],
+    [sg.In(size=(25, 1), enable_events=True, key="-PASSWORD-")],
+    [sg.HSeparator()],
+    [sg.FolderBrowse(), sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-FILE LIST-")],
+]
 
+# For now will only show the name of the file that was chosen
+right_column = [
+    [sg.Text("Choose an image from list on left:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")],
+]
 
-def window():
-    app = QApplication(sys.argv)
-    w = QWidget()
-    b = QLabel(w)
-    b.setText("Hello World!")
-    w.setGeometry(100, 100, 200, 50)
-    b.move(50, 20)
-    w.setWindowTitle("Ultimate Guitar Downloader")
-    w.show()
-    sys.exit(app.exec_())
+# ----- Full layout -----
+layout = [
+    [
+        sg.Column(left_column),
+        sg.VSeperator(),
+        sg.Column(right_column),
+    ]
+]
 
+window = sg.Window("Ultimate Guitar Downloader", layout)
 
+while True:
+    event, values = window.read()
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
 
+window.close()
