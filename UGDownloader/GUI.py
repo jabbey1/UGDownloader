25777bca6cb107af66244a01494304f8764c6e4e
jake <jake.c.abbey@gmail.com>
2022-08-11 00:19:59 -0700

    Working on captcha

--- UGDownloader/GUI.py
+++ UGDownloader/GUI.py
@@ -143,11 +143,26 @@ def login(driver, user, password):
     password_textbox.send_keys(password)
     password_textbox.send_keys(Keys.RETURN)
     # todo deal with captcha here
-    # for _ in xrange(100):  # or loop forever, but this will allow it to timeout if the user falls asleep or whatever
+    # captcha css selectors
+    # #captchak8gPWM_TLJs0GssOrg0gG > div:nth-child(1) > div:nth-child(1) > iframe:nth-child(1)
+    # title="reCAPTCHA"
+    # todo convert this java code
+    # WebDriverWait(driver, 10).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
+    #     By.xpath("//iframe[starts-with(@name, 'a-') and starts-with(@src, 'https://www.google.com/recaptcha')]")));
+    #
+    # WebDriverWait(driver, 20).until(
+    #     ExpectedConditions.elementToBeClickable(By.cssSelector("div.recaptcha-checkbox-checkmark"))).click();
+
+    # for _ in range(100):  # or loop forever, but this will allow it to timeout if the user falls asleep or whatever
     #     if driver.get_current_url.find("captcha") == -1:
     #         break
     #     time.sleep(6)  # wait 6 seconds which means the user has 10 minutes before timeout occurs
 
+    # todo another possible method:
+    WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[src^='https://www.google.com/recaptcha/api2/anchor?']")))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "span.recaptcha-checkbox.goog-inline-block.recaptcha-checkbox-unchecked.rc-anchor-checkbox"))).click()
+    driver.switch_to_default_content()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn.btn-primary.block.full-width.m-b"))).click()
     time.sleep(.5)
     # this popup sometimes takes some time to appear, wait until it's clickable
     element = WebDriverWait(driver, 20).until(
--- UGDownloader/main.py
+++ UGDownloader/main.py
@@ -3,8 +3,12 @@ import GUI
 # todo test headless. does window need to be a certain size? restrict resize?
 # testing
 artist = 'Wormrot'
-user = 'mygoodusername'
-password = 'passyword'
+# account 1
+# user = 'mygoodusername'
+# password = 'passyword'
+# account 2
+user = 'jake.c.abbey'
+password = '!bRD3*@erWRZ54'
 driver = GUI.start_browser(artist)
 GUI.start_download(driver, artist, user, password)
 
